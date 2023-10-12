0d3e5b877184b709b710bb9247a36468e1ed54e9
jinsollee-dev <147626882+jinsollee-dev@users.noreply.github.com>
2023-10-12 09:16:21 +0900

    If_test
    test if

---
+++ If_test.java
@@ -0,0 +1,56 @@
+package practice;
+
+import java.util.Scanner;
+
+public class If_test {
+
+	public static void main(String[] args) {
+//		//Q1.
+//		System.out.println("두 정수를 입력하세요.");
+//		Scanner scanner = new Scanner(System.in); 
+//		int a = scanner.nextInt();
+//		int b = scanner.nextInt();
+//		System.out.println(a+"+"+b+"은 "+(a+b));
+	    	
+//		//Q2. 
+//		Scanner scanner = new Scanner(System.in); 
+//		int state = scanner.nextInt();
+//		int height = state * 5; 
+//		System.out.println(height+"m"+" 입니다.");
+//		
+//	    	
+//		//Q3. 
+//		System.out.println("X값을 입력하세요.");
+//		Scanner s1 = new Scanner(System.in);
+//		int x = s1.nextInt();
+//		int y = (x*x)-3*x+7; 
+//		System.out.println("x="+x+", "+ "y="+y);
+//	
+//		//Q5. 
+//		System.out.println("논리 연산을 입력하세요.");
+//		 Scanner s1 = new Scanner(System.in);
+//			boolean a = s1.nextBoolean();
+//			String op = s1.next();
+//			boolean b = s1.nextBoolean(); 
+//			switch(op){
+//				case "AND": 
+//				if (a == true && b == true){
+//					System.out.println("true");
+//				} System.out.println("false");
+//					break; 
+//				case "OR" : 
+//					if (a== false && b == false) {
+//						System.out.println("false");
+//					  }System.out.println("true");
+//					break;} 				
+//				
+			
+//			switch(op){
+//			case "AND" : System.out.println(a && b);break; 
+//			case "OR" : System.out.println(a || b); break; 
+//			}			
+		
+		
+			
+	}	
+}
